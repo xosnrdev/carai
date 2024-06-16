af8cde524248aafd0745080af69c298105068eb2
xosnrdev <successxodev@gmail.com>
2024-06-16 18:15:33 -0400

    ci: removal of fetch depth on all system nightly builds

--- editor/next.config.mjs
+++ editor/next.config.mjs
@@ -1,4 +1,3 @@
-import { withSentryConfig } from '@sentry/nextjs'
 
 /**
  * @type {import('next').NextConfig}
@@ -17,41 +16,3 @@ export async function rewrites() {
 	]
 }
 
-export default withSentryConfig(
-	withSentryConfig(nextConfig, {
-		// For all available options, see:
-		// https://github.com/getsentry/sentry-webpack-plugin#options
-
-		org: 'xosnrdev',
-		project: 'carai-editor',
-
-		authToken: process.env.SENTRY_AUTH_TOKEN,
-
-		// Only print logs for uploading source maps in CI
-		silent: !process.env.CI,
-
-		// For all available options, see:
-		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-		// Upload a larger set of source maps for prettier stack traces (increases build time)
-		widenClientFileUpload: true,
-
-		// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-		// This can increase your server load as well as your hosting bill.
-		// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-		// side errors will fail.
-		// tunnelRoute: "/monitoring",
-
-		// Hides source maps from generated client bundles
-		hideSourceMaps: true,
-
-		// Automatically tree-shake Sentry logger statements to reduce bundle size
-		disableLogger: true,
-
-		// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-		// See the following for more information:
-		// https://docs.sentry.io/product/crons/
-		// https://vercel.com/docs/cron-jobs
-		automaticVercelMonitors: false,
-	})
-)
