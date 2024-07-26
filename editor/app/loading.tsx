614d8b7074dbb68bacd89179ef212deaa82470c1
xosnrdev <successxodev@gmail.com>
2024-07-25 23:53:51 -0400

    feat: added new loading component

---
+++ editor/app/loading.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react'
+
+import { LoadingSpinner } from '@/components/ui/icons'
+
+const Loading: FC = () => {
+    return <LoadingSpinner size={75} />
+}
+
+export default Loading
