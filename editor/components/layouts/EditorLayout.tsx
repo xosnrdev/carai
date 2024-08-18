62144a93c0e097bbf73b008005d7ce195f881527
xosnrdev <successxodev@gmail.com>
2024-08-18 13:32:09 -0400

    refactor: Improve layout structure and remove unused code

--- editor/components/layouts/EditorLayout.tsx
+++ editor/components/layouts/EditorLayout.tsx
@@ -1,17 +1,18 @@
 'use client'
 
 import { Button } from '@nextui-org/button'
-import { redirect, usePathname } from 'next/navigation'
-import { useEffect, useRef, type FC, type ReactNode } from 'react'
+import { type FC, type ReactNode } from 'react'
 import { Toaster } from 'react-hot-toast'
 
 import useAppContext from '@/hooks/useAppContext'
-import { cn } from '@/lib/utils'
+import useDeviceSupportCheck from '@/hooks/useDeviceSupportCheck'
 import useTabContext from '@/hooks/useTabContext'
+import { cn } from '@/lib/utils'
 
 import CustomTooltip from '../ui/custom-tooltip'
 import Header from '../ui/header'
 import Sidebar from '../ui/sidebar'
+import UnsupportedDevice from '../ui/unsupported-device'
 
 export interface IMainLayoutProps {
     children: ReactNode
@@ -20,52 +21,39 @@ export interface IMainLayoutProps {
 const MainLayout: FC<IMainLayoutProps> = ({ children }) => {
     const { isOpen, setIsOpen } = useAppContext()
     const { activeTab } = useTabContext()
-    const pathname = usePathname()
-    const containerRef = useRef<HTMLDivElement | null>(null)
 
-    useEffect(() => {
-        const container = containerRef.current
+    const isSupportedDevice = useDeviceSupportCheck()
 
-        if (!container) return
+    if (!isSupportedDevice) {
+        return <UnsupportedDevice />
+    }
 
-        const checkScreenSize = (width: number) => {
-            const minWidth = 640
-
-            if (width < minWidth && pathname !== '/unsupported') {
-                redirect('/unsupported')
-            } else if (width >= minWidth && pathname === '/unsupported') {
-                redirect('/')
-            }
-        }
-
-        const resizeObserver = new ResizeObserver((entries) => {
-            for (const entry of entries) {
-                const { width } = entry.contentRect
-
-                checkScreenSize(width)
-            }
-        })
-
-        resizeObserver.observe(container)
-
-        checkScreenSize(container.offsetWidth)
-
-        return () => {
-            resizeObserver.disconnect()
-        }
-    }, [pathname])
+    if (!activeTab) {
+        return (
+            <div className="custom-scrollbar relative flex h-dvh flex-col overflow-hidden border-x border-default">
+                <Header />
+                <Toaster position="top-center" reverseOrder={true} />
+                <div className="flex h-full flex-row overflow-hidden">
+                    <div>
+                        <Sidebar />
+                    </div>
+                    <div className="h-full flex-1 bg-[#FFFFFF] dark:bg-[#1E1E2A]">
+                        {children}
+                    </div>
+                </div>
+                <div className="sticky h-12 border-y border-default" />
+            </div>
+        )
+    }
 
     return (
-        <div
-            ref={containerRef}
-            className="custom-scrollbar relative flex h-dvh flex-col overflow-hidden bg-background"
-        >
+        <div className="custom-scrollbar relative flex h-dvh flex-col overflow-hidden border-x border-default">
             <Header />
             <Toaster position="top-center" reverseOrder={true} />
             <div className="flex h-full flex-row overflow-hidden">
                 <div
                     className={cn({
-                        hidden: isOpen.sidebar === false,
+                        hidden: !isOpen.sidebar,
                     })}
                 >
                     <Sidebar />
@@ -87,7 +75,7 @@ const MainLayout: FC<IMainLayoutProps> = ({ children }) => {
                     <Button
                         isIconOnly
                         className={cn('absolute left-0 top-9 z-50 text-2xl', {
-                            'bg-default': isOpen.sidebar === false,
+                            'bg-default': !isOpen.sidebar,
                             'ml-2': isOpen.sidebar,
                         })}
                         radius="none"
