ec9e1530d6f1e7b0c5224af4b8d8da90dfd6900b
xosnrdev <successxodev@gmail.com>
2024-08-28 16:32:17 -0400

    chore: file reorganizing and migration to RCE Engine

--- editor/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react'
-
-import { LoadingSpinner } from '@/components/ui/icons'
-
-const Loading: FC = () => {
-    return <LoadingSpinner size={75} />
-}
-
-export default Loading
--- editor/components/ui/custom-tooltip.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react'
-
-import { Tooltip, type TooltipProps } from '@nextui-org/tooltip'
-
-const CustomTooltip: FC<TooltipProps> = ({ ...props }) => {
-    return <Tooltip {...props} offset={-7} radius="none" size="sm" />
-}
-
-export default CustomTooltip
