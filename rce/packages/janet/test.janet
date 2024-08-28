8072c503eeb45d41557487a527dfa9472683bf8b
xosnrdev <successxodev@gmail.com>
2024-08-28 16:24:53 -0400

    refactor: Remove previous RCE inplementation prior to new migration to RCE Engine
    This commit removes several unused files and directories from the codebase. It deletes the .nvmrc file, .nycrc file, test files for various programming languages, editorconfig file, install scripts for V, Java, and TypeScript-Bun, and a Brainfuck code file. These changes improve the codebase by reducing clutter and removing unnecessary files that are no longer needed.

--- rce/packages/janet/test.janet
+++
@@ -1,3 +0,0 @@
-# Prints hello
-
-(print "Hello world!")
--- rce/packages/java/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd ~
-
-curl -LO https://download.oracle.com/java/17/archive/jdk-17.0.6_linux-x64_bin.tar.gz
-tar -zxf jdk-17.0.6_linux-x64_bin.tar.gz
-mkdir -p /opt/java/
-mv -v jdk-17.0.6/ /opt/java/17/
-rm -rf jdk-17.0.6_linux-x64_bin.tar.gz
