aa238d763e40e8d1952e1fd521a5ef2fbf60070f
xosnrdev <successxodev@gmail.com>
2024-08-28 16:36:37 -0400

    chore: Organize files and add necessary configuration files

---
+++ sentry.client.config.ts
@@ -0,0 +1,15 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+    debug: false,
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        Sentry.replayIntegration({
+            maskAllText: true,
+            blockAllMedia: true,
+        }),
+    ],
+    replaysOnErrorSampleRate: 1.0,
+    replaysSessionSampleRate: 0.05,
+    tracesSampleRate: 0.5,
+})
