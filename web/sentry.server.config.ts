94296b07dbf08fbadbce1f7b6f77409ffbbb62e4
xosnrdev <successxodev@gmail.com>
2024-09-09 22:09:44 -0400

    refactor: Restructured folder architecture

---
+++ web/sentry.server.config.ts
@@ -0,0 +1,8 @@
+import * as Sentry from '@sentry/nextjs'
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 0.5,
+    debug: false,
+    spotlight: process.env.NODE_ENV === 'development',
+})
