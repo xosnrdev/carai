e79d357a1428b07cb1cac4d1311914b69026c6f5
xosnrdev <successxodev@gmail.com>
2024-09-20 23:19:54 -0400

    refactor: migrate to generate images using code (.js, .ts, .tsx) for open graph images see https://nextjs.org/docs/app/api-reference/file-conventions/metadata/opengraph-image#generate-images-using-code-js-ts-tsx for reference

---
+++ web/app/(landing)/opengraph-image.tsx
@@ -0,0 +1,49 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+export const alt = 'Carai Open Graph Image'
+export const size = {
+    width: 1200,
+    height: 630,
+}
+
+export const contentType = 'image/png'
+
+export default async function Image() {
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    fontSize: 128,
+                    background: '#2f2f3b',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <svg
+                    className="lucide lucide-braces"
+                    fill="none"
+                    height="24"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    width="24"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1" />
+                    <path d="M16 21h1a2 2 0 0 0 2-2v-5c0-1.1.9-2 2-2a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2h-1" />
+                </svg>{' '}
+                <span>Carai</span>
+            </div>
+        ),
+        {
+            ...size,
+        }
+    )
+}
