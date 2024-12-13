f405eb527b725c25a4ffbdd890d9dedd17354234
xosnrdev <successxodev@gmail.com>
2024-12-12 20:18:17 -0500

    refactor(dto): organize modules

--- server/src/dto/auth.rs
+++ server/src/dto/auth.rs
@@ -2,17 +2,36 @@ use serde::Deserialize;
 use validator::Validate;
 
 #[derive(Debug, Deserialize, Validate)]
-pub struct UpdateProfileDto {
+pub struct RegisterDto {
     #[validate(length(min = 3, max = 30))]
     #[serde(default, deserialize_with = "super::to_lowercase_optional")]
     pub username: Option<String>,
+
     #[validate(email, length(max = 320))]
     #[serde(default, deserialize_with = "super::to_lowercase_optional")]
     pub email: Option<String>,
+
     #[validate(length(min = 8, max = 128))]
+    pub password: String,
+
     #[serde(default)]
-    pub password: Option<String>,
+    pub github_id: Option<i64>,
+
     #[validate(url)]
     #[serde(default)]
     pub avatar_url: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Validate)]
+pub struct LoginDto {
+    #[validate(length(min = 3, max = 30))]
+    #[serde(default, deserialize_with = "super::to_lowercase_optional")]
+    pub username: Option<String>,
+
+    #[validate(email, length(max = 320))]
+    #[serde(default, deserialize_with = "super::to_lowercase_optional")]
+    pub email: Option<String>,
+
+    #[validate(length(min = 8, max = 128))]
+    pub password: String,
 }
