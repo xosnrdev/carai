855e938ca0f728d02732688013852ccd236491bf
xosnrdev <successxodev@gmail.com>
2024-12-12 20:24:19 -0500

    refactor(response): enhance error handling and response structure
    - Updated AppError to use StatusCode instead of u16 for better type safety.
    - Improved internal and external message handling for error responses.
    - Added cache control and security headers to error and success responses.
    - Introduced new response methods for created and no content statuses in SuccessResponse.

--- server/src/response.rs
+++ server/src/response.rs
@@ -1,4 +1,5 @@
 use axum::{
+    http::{header, HeaderValue, StatusCode},
     response::{IntoResponse, Response},
     Json,
 };
@@ -14,37 +15,37 @@ impl Message {
     pub fn to_internal(&self) -> String {
         match self {
             Message::Internal(err) => err.to_string(),
-            _ => "An internal server error occurred.".to_string(),
+            _ => "An internal error occurred".to_owned(),
         }
     }
 
     pub fn to_external(&self) -> String {
         match self {
             Message::External(msg) => msg.to_owned(),
-            Message::Internal(msg) => msg.to_string(),
+            Message::Internal(err) => err.to_string(),
         }
     }
 }
 
 #[derive(Debug)]
 pub struct AppError {
-    status: u16,
+    status: StatusCode,
     message: Message,
 }
 
 impl AppError {
-    pub fn new(status: u16, message: Message) -> Self {
+    pub fn new(status: StatusCode, message: Message) -> Self {
         Self { status, message }
     }
 
     pub fn internal(error: impl Into<anyhow::Error>) -> Self {
         Self {
-            status: 500,
+            status: StatusCode::INTERNAL_SERVER_ERROR,
             message: Message::Internal(error.into()),
         }
     }
 
-    pub fn external(message: impl Into<String>, status: u16) -> Self {
+    pub fn external(message: impl Into<String>, status: StatusCode) -> Self {
         Self {
             status,
             message: Message::External(message.into()),
@@ -53,7 +54,7 @@ impl AppError {
 
     pub fn to_error_response(&self) -> ErrorResponse {
         ErrorResponse {
-            status: self.status,
+            status: self.status.as_u16(),
             body: self.message.to_external(),
         }
     }
@@ -61,7 +62,33 @@ impl AppError {
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        Json(self.to_error_response()).into_response()
+        let error_response = Json(self.to_error_response()).into_response();
+
+        let mut response = error_response;
+        *response.status_mut() = self.status;
+
+        let headers = response.headers_mut();
+        headers.insert(header::CACHE_CONTROL, HeaderValue::from_static("no-store"));
+        headers.insert(header::PRAGMA, HeaderValue::from_static("no-cache"));
+        headers.insert(
+            header::STRICT_TRANSPORT_SECURITY,
+            HeaderValue::from_static("max-age=31536000; includeSubDomains; preload"),
+        );
+        headers.insert(
+            header::X_CONTENT_TYPE_OPTIONS,
+            HeaderValue::from_static("nosniff"),
+        );
+        headers.insert(header::X_FRAME_OPTIONS, HeaderValue::from_static("DENY"));
+        headers.insert(
+            header::X_XSS_PROTECTION,
+            HeaderValue::from_static("1; mode=block"),
+        );
+        headers.insert(
+            header::REFERRER_POLICY,
+            HeaderValue::from_static("no-referrer"),
+        );
+
+        response
     }
 }
 
@@ -79,11 +106,44 @@ impl<T: Serialize> SuccessResponse<T> {
     pub fn ok(body: T) -> Self {
         Self::new(body, 200)
     }
+
+    pub fn created(body: T) -> Self {
+        Self::new(body, 201)
+    }
+
+    pub fn no_content(body: T) -> Self {
+        Self::new(body, 204)
+    }
 }
 
 impl<T: Serialize> IntoResponse for SuccessResponse<T> {
     fn into_response(self) -> Response {
-        Json(self).into_response()
+        let mut response = Json(&self).into_response();
+
+        *response.status_mut() = StatusCode::from_u16(self.status).unwrap();
+
+        let headers = response.headers_mut();
+        headers.insert(header::CACHE_CONTROL, HeaderValue::from_static("no-store"));
+        headers.insert(header::PRAGMA, HeaderValue::from_static("no-cache"));
+        headers.insert(
+            header::STRICT_TRANSPORT_SECURITY,
+            HeaderValue::from_static("max-age=31536000; includeSubDomains; preload"),
+        );
+        headers.insert(
+            header::X_CONTENT_TYPE_OPTIONS,
+            HeaderValue::from_static("nosniff"),
+        );
+        headers.insert(header::X_FRAME_OPTIONS, HeaderValue::from_static("DENY"));
+        headers.insert(
+            header::X_XSS_PROTECTION,
+            HeaderValue::from_static("1; mode=block"),
+        );
+        headers.insert(
+            header::REFERRER_POLICY,
+            HeaderValue::from_static("no-referrer"),
+        );
+
+        response
     }
 }
 
@@ -97,7 +157,6 @@ macro_rules! process_error_from {
     ($error:ty) => {
         impl From<$error> for AppError {
             fn from(error: $error) -> Self {
-                tracing::debug!("{}", error);
                 AppError::internal(error)
             }
         }
