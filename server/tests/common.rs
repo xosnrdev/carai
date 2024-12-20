0cb9d98af882248f88af2390d6b8da22bfc71890
xosnrdev <successxodev@gmail.com>
2024-12-20 18:25:49 -0500

    fix(tests): load environment variables

--- server/tests/common.rs
+++ server/tests/common.rs
@@ -7,6 +7,7 @@ use carai::{
 use sqlx::PgPool;
 
 pub fn ctx(db_pool: PgPool) -> CaraiResult<Router> {
+    dotenv::dotenv().ok();
     let config = AppConfig::new()?;
 
     Ok(create_router(db_pool, config))
